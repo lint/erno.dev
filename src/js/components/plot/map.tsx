f69e51f925226ec478a55bbdf4167acca8580334
lint <47455468+lint@users.noreply.github.com>
2024-12-29 18:53:00 -0800

    remove testing code

--- src/js/components/plot/map.tsx
+++ src/js/components/plot/map.tsx
@@ -1,4 +1,3 @@
-import * as d3 from 'd3';
 import { FeatureCollection } from 'geojson';
 import React, { useState } from 'react';
 import './map.css';
@@ -6,44 +5,13 @@ import './map.css';
 type MapProps = {
     width: number;
     height: number;
-    data: FeatureCollection;
 };
 
-export function Map({ width, height, data }: MapProps) {
-
-    const projection = d3
-        .geoMercator()
-        .scale(width / 2 / Math.PI - 40)
-        .center([10, 35]);
-
-    const geoPathGenerator = d3.geoPath().projection(projection);
-
-    const allSvgPaths = data.features
-    // .filter((shape) => shape.id !== 'ATA')
-    .map((shape) => {
-        let path = geoPathGenerator(shape);
-
-        if (path == null) {
-            return;
-        }
-
-        return (
-            <path
-                key={shape.id}
-                d={path}
-                stroke="lightGrey"
-                // strokeWidth={0.5}
-                fill="grey"
-                // fillOpacity={0.7}
-            />
-        );
-    });
+export function Map({ width, height }: MapProps) {
 
     return (
         <div>
-            <svg className="map" width={width} height={height}>
-                {allSvgPaths}
-            </svg>
+
         </div>
     );
 }
